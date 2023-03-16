3559e0165ce0796e292bad2dac86a0c782f0df94
kheisler4417 <kheisler4417@gmail.com>
2023-03-15 22:47:42 -0400

    final

--- guessing-game.js
+++ guessing-game.js
@@ -1,16 +1,130 @@
-const readline = require("readline"); //construct the readline
+// const readline = require("readline"); //construct the readline
 
-const rl = readline.createInterface({ //create the interface
-    input: process.stdin,
-    output: process.stdout
-});
+// const rl = readline.createInterface({ //create the interface
+//     input: process.stdin,
+//     output: process.stdout
+// });
 //equivalant:::
 // const readline = require('readline').createInterface({
 //     input: process.stdin,
 //     output: process.stdout
 // });
 
-let secretNumber = 7;
+// const readline = require('readline');
+
+// function guessNumber() {
+//     const rl = readline.createInterface({
+//         input: process.stdin,
+//         output: process.stdout
+//     });
+
+//     rl.question('Enter a max number: ', (maxNumber) => {
+//         rl.question('Enter a min number: ', (minNumber) => {
+//             const secretNumber = Math.floor(Math.random() * (maxNumber - minNumber + 1)) + parseInt(minNumber);
+//             console.log(`I'm thinking of a number between ${minNumber} and ${maxNumber}...`);
+
+//             let numTries = 0;
+
+//             const getGuess = () => {
+//                 rl.question('Enter a guess: ', (guess) => {
+//                     guess = parseInt(guess);
+//                     if (guess === secretNumber) {
+//                         console.log(`Correct! It took you ${numTries} tries.`);
+//                         console.log('YOU WON.');
+//                         rl.close();
+//                     } else if (guess < secretNumber) {
+//                         console.log('Too low.');
+//                         numTries++;
+//                         getGuess();
+//                     } else {
+//                         console.log('Too high.');
+//                         numTries++;
+//                         getGuess();
+//                     }
+//                 });
+//             };
+
+//             getGuess();
+//         });
+//     });
+// }
+
+// guessNumber();
+
+const readline = require('readline');
+
+function guessNumber() {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    function getSecretNumber(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    function displayMessage(message) {
+        console.log(message);
+    }
+
+    function getGuess(numTries, secretNumber) {
+        rl.question('Enter a guess: ', (guess) => {
+            guess = parseInt(guess);
+            if (guess === secretNumber) {
+                console.log(`Correct! It took you ${numTries} tries.`);
+                console.log('YOU WON.');
+                rl.close();
+            } else if (guess < secretNumber) {
+                displayMessage('Too low.');
+                numTries++;
+                getGuess(numTries, secretNumber);
+            } else {
+                displayMessage('Too high.');
+                numTries++;
+                getGuess(numTries, secretNumber);
+            }
+        });
+    }
+
+    rl.question('Enter a max number: ', (maxNumber) => {
+        rl.question('Enter a min number: ', (minNumber) => {
+            const secretNumber = getSecretNumber(parseInt(minNumber), parseInt(maxNumber));
+            displayMessage(`I'm thinking of a number between ${minNumber} and ${maxNumber}...`);
+
+            let numTries = 0;
+            getGuess(numTries, secretNumber);
+        });
+    });
+}
+
+guessNumber();
+
+
+// this only works in the browser:
+// function guessNumber() {
+//     const maxNumber = parseInt(prompt("Enter a max number: "));
+//     const minNumber = parseInt(prompt("Enter a min number: "));
+//     const secretNumber = Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
+//     console.log(`I'm thinking of a number between ${minNumber} and ${maxNumber}...`);
+
+//     let guess = parseInt(prompt("Enter a guess: "));
+//     let numTries = 1;
+
+//     while (guess !== secretNumber) {
+//         if (guess < secretNumber) {
+//             guess = parseInt(prompt("Too low. Enter a guess: "));
+//         } else {
+//             guess = parseInt(prompt("Too high. Enter a guess: "));
+//         }
+//         numTries++;
+//     }
+
+//     console.log(`Correct! It took you ${numTries} tries.`);
+//     console.log("YOU WON.");
+// }
+
+// guessNumber();
+
 
 // function checkGuess(guess) {
 //     if (guess > secretNumber) {
@@ -35,12 +149,12 @@ let secretNumber = 7;
 
 
 
-// const readline = require("readline"); //construct the readline
+                            // const readline = require("readline"); //construct the readline
 
-// const rl = readline.createInterface({ //create the interface
-//     input: process.stdin,
-//     output: process.stdout
-// });
+                            // const rl = readline.createInterface({ //create the interface
+                            //     input: process.stdin,
+                            //     output: process.stdout
+                            // });
 
 // rl.question("What's up, doc? ", handleResponseOne);
 
